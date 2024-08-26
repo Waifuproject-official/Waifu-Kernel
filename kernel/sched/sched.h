615b50b249f6741750341aeb862fbf4f9533def9
y82t2z <nyakoriandnyarutoru@gmail.com>
2024-08-26 14:51:32 +0700

    Fix kernel/sched/sched.h

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1464,19 +1464,7 @@ static inline void __set_task_cpu(struct task_struct *p, unsigned int cpu)
 #endif
 }
 
-<<<<<<< HEAD
-/*
- * Tunables that become constants when CONFIG_SCHED_DEBUG is off:
- */
-#ifdef CONFIG_SCHED_DEBUG
-# include <linux/static_key.h>
-# define const_debug __read_mostly
-#else
-# define const_debug const
-#endif
-=======
 #define const_debug __read_mostly
->>>>>>> 1a16b4e8a0ca (sched/fair: Promote sched_migration_cost_ns out of CONFIG_SCHED_DEBUG)
 
 #define SCHED_FEAT(name, enabled)	\
 	__SCHED_FEAT_##name ,
