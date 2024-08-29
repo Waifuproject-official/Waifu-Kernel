b8e4600b8d23c62b7508f30f87775d6fa9476947
y82t2z <nyakoriandnyarutoru@gmail.com>
2024-08-29 08:10:15 +0700

    Fix Build

--- drivers/gpu/drm/msm/sde/sde_crtc.c
+++ drivers/gpu/drm/msm/sde/sde_crtc.c
@@ -21,6 +21,7 @@
 #include <linux/debugfs.h>
 #include <linux/ktime.h>
 #include <linux/devfreq_boost.h>
+#include <linux/cpu_input_boost.h>
 #include <uapi/drm/sde_drm.h>
 #include <drm/drm_mode.h>
 #include <drm/drm_crtc.h>
