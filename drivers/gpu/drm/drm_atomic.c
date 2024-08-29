75d1df7f6cf26aedc588297170ce9e1338dc77fe
y82t2z <nyakoriandnyarutoru@gmail.com>
2024-08-29 07:53:01 +0700

    Fixed Build

--- drivers/gpu/drm/drm_atomic.c
+++ drivers/gpu/drm/drm_atomic.c
@@ -33,7 +33,7 @@
 #include <linux/pm_qos.h>
 #include <linux/sync_file.h>
 #include <linux/devfreq_boost.h>
-
+#include <linux/cpu_input_boost.h>
 #include "drm_crtc_internal.h"
 
 void __drm_crtc_commit_free(struct kref *kref)
